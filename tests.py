116d0b28969ace1b04b9d23cc93deb3125eefb95
RationalFlaneur1983 <ashikhmatov@gmail.com>
2024-11-26 21:41:30 +0600

    moved fixtures to conftest.py amended tests.py based on reviewer's comments

--- tests.py
+++ tests.py
@@ -16,7 +16,7 @@ class TestBooksCollector:
 
     def test_add_new_book_name_over_40(self, collector):
         collector.add_new_book("A" * 41)
-        assert "A" * 41 not in collector.books_genre
+        assert  len(collector.get_books_genre()) == 0
 
 # Тест для set_book_genre
     def test_set_book_genre_success(self, collector):
@@ -32,10 +32,11 @@ class TestBooksCollector:
     def test_get_book_genre_existing(self, collector):
         collector.add_new_book('Гордость, и предубеждение, и зомби')
         collector.set_book_genre('Гордость, и предубеждение, и зомби', 'Фантастика')
-        assert collector.get_book_genre('Гордость, и предубеждение, и зомби') == 'Фантастика'
+        assert collector.get_books_genre()['Гордость, и предубеждение, и зомби'] == 'Фантастика'
 
     def test_get_book_genre_not_exist(self, collector):
-        assert collector.get_book_genre('Нет такой книги') is None
+        collector.add_new_book('Книга без жанра')
+        assert collector.get_book_genre('Книга без жанра') == ''
 
 # Тест для get_books_with_specific_genre
     def test_get_books_with_specific_genre(self, collector):
@@ -58,7 +59,7 @@ class TestBooksCollector:
     def test_get_books_for_children(self, collector):
         collector.add_new_book('Гордость, и предубеждение, и зомби')
         collector.set_book_genre('Гордость, и предубеждение, и зомби', 'Фантастика')
-        collector.add_new_book('Гордость, и предубеждение, и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Ужасы')
         assert collector.get_books_for_children() == ['Гордость, и предубеждение, и зомби']
 
@@ -77,6 +78,8 @@ class TestBooksCollector:
 
 # Тест для get_list_of_favorites_books
     def test_get_list_of_favorites_books(self, collector):
+        collector.add_new_book('Еще одна книга для массовки')
         collector.add_new_book('Гордость, и предубеждение, и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.add_book_in_favorites('Гордость, и предубеждение, и зомби')
         assert collector.get_list_of_favorites_books() == ['Гордость, и предубеждение, и зомби']
